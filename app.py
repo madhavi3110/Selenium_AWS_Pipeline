e45d52d3eebd4c6ea182bba3e0b17f8caa4d6be2
madhavi3110 <47594154+madhavi3110@users.noreply.github.com>
2022-07-20 21:11:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class app_test_case(unittest.TestCase):
     def setUp(self):
         chromeOptions = webdriver.ChromeOptions()
         #driver_path = '/usr/local/bin/chromedriver'
-        river_path = "/opt/chromedriver"
+        driver_path = "/opt/chromedriver"
         chromeOptions.add_argument('--headless')
         chromeOptions.add_argument('--disable-gpu')
         chromeOptions.add_argument('--no-sandbox')
