e9f4fda307a45ad000833621ae1b82fdf59c1641
madhavi3110 <47594154+madhavi3110@users.noreply.github.com>
2022-07-26 18:53:26 +0530

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -1,14 +1,32 @@
-# Download and Install chromedriver
-wget -N https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+#!/usr/bin/bash
 
+declare -A chrome_versions
 
-# Install chrome broswer
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+# Enter the list of browsers to be downloaded
+### Using Chromium as documented here - https://www.chromium.org/getting-involved/download-chromium
+chrome_versions=( ['88.0.4324.150']='827102' ['89.0.4389.47']='843831' )
+chrome_drivers=( "88.0.4324.96" "89.0.4389.23" )
+
+# Download Chrome
+for br in "${!chrome_versions[@]}"
+do
+    echo "Downloading Chrome version $br"
+    mkdir -p "/opt/chrome/$br"
+    curl -Lo "/opt/chrome/$br/chrome-linux.zip" "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${chrome_versions[$br]}%2Fchrome-linux.zip?alt=media"
+    unzip -q "/opt/chrome/$br/chrome-linux.zip" -d "/opt/chrome/$br/"
+    mv /opt/chrome/$br/chrome-linux/* /opt/chrome/$br/
+    rm -rf /opt/chrome/$br/chrome-linux "/opt/chrome/$br/chrome-linux.zip"
+done
+
+# Download Chromedriver
+for dr in ${chrome_drivers[@]}
+do
+    echo "Downloading Chromedriver version $dr"
+    mkdir -p "/opt/chromedriver/$dr"
+    curl -Lo "/opt/chromedriver/$dr/chromedriver_linux64.zip" "https://chromedriver.storage.googleapis.com/$dr/chromedriver_linux64.zip"
+    unzip -q "/opt/chromedriver/$dr/chromedriver_linux64.zip" -d "/opt/chromedriver/$dr/"
+    chmod +x "/opt/chromedriver/$dr/chromedriver"
+    rm -rf "/opt/chromedriver/$dr/chromedriver_linux64.zip"
+done
 apt-get -y update
 apt-get -y install google-chrome-stable
